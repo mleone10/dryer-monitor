146abb8aa7e992874e784d89a711bca4c2d00352
Mario Leone <mleone10@gmail.com>
2018-12-21 13:25:11 +0000

    Adding GPIO setup

--- dryer_monitor.py
+++ dryer_monitor.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import RPi.GPIO as GPIO
 
-def main(debug=False):
-    return debug
+input_channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input_channel, GPIO.IN)
+
+def main():
+    return isVibrating()
+
+def isVibrating():
+    return GPIO.input(input_channel)
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch
 import dryer_monitor
 
 class TestDryerMonitor(unittest.TestCase):
-    def test_debug(self):
-        self.assertEqual(dryer_monitor.main(True), True)
+    @patch('dryer_monitor.RPi')
+    @patch('dryer_monitor.RPi.GPIO')
+    def test_debug(self, mock_gpio, mock_rpi):
+        mock_gpio.input.return_value = True
+        self.assertEqual(dryer_monitor.main(), True)
 
 if __name__ == '__main__':
     unittest.main()
