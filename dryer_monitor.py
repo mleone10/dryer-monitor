ad2b13db376377187ffe3896625d0bdd7dc1ecbb
Mario Leone <mleone10@gmail.com>
2018-12-23 04:08:47 +0000

    Adding sensing logic.

--- dryer_monitor.py
+++ dryer_monitor.py
@@ -1,15 +1,50 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
+import time
 
 input_channel = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(input_channel, GPIO.IN)
+SENSING_SECONDS_MAX = 300
+
+class Waiting():
+    def next(self):
+        if isVibrating():
+            return Sensing()
+        else:
+            return self
+
+class Sensing():
+    sensing_seconds_current = 0
+    def next(self):
+        if self.sensing_seconds_current == SENSING_SECONDS_MAX:
+            return Drying()
+        elif isVibrating():
+            self.sensing_seconds_current += 1
+            return self
+        else:
+            return Waiting()
+
+class Drying():
+    def next(self):
+        if isVibrating():
+            return self
+        else:
+            notify()
+            return Waiting()
 
 def main():
-    return isVibrating()
+    state = Waiting()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(input_channel, GPIO.IN)
+
+    while True:
+        time.sleep(1)
+        state = state.next()
 
 def isVibrating():
     return GPIO.input(input_channel)
+
+def notify():
+    print("Sending text!")
 
 if __name__ == '__main__':
     print(main())
--- test.py
+++ test.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 import dryer_monitor
 
 class TestDryerMonitor(unittest.TestCase):
     @patch('dryer_monitor.GPIO')
-    def test_override_gpio_input(self, mock_gpio):
-        mock_gpio.input.return_value = True
-        self.assertEqual(dryer_monitor.main(), True)
+    def test_happy_path(self, mock_gpio):
+        dryer_monitor.SENSING_SECONDS_MAX = 3
+        mock_gpio.input.side_effect = [
+                False, False, True, True, True, True, False
+                ]
+
+        state = dryer_monitor.Waiting()
+        for i in range(0, 8):
+            state = state.next()
+
+    @patch('dryer_monitor.GPIO')
+    def test_stops_before_drying(self, mock_gpio):
+        dryer_monitor.SENSING_SECONDS_MAX = 3
+        mock_gpio.input.side_effect = [
+                False, False, True, True, False
+                ]
+
+        state = dryer_monitor.Waiting()
+        for i in range(0, 5):
+            state = state.next()
 
 if __name__ == '__main__':
     unittest.main()
